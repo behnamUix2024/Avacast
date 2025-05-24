4e9c51f617c7eca835473f99de8fffa856b18002
Behnam-6.403 <behnamuiux2023@gmail.com>
2025-05-25 00:22:17 +0330

    first commit

--- app/src/main/java/com/behnamuix/avacast/MainActivity.kt
+++ app/src/main/java/com/behnamuix/avacast/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.behnamuix.avacast
 
+import android.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.behnamuix.avacast.ui.theme.AvacastTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +39,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AvacastTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Column {
+                //oriantation:Vertical
+                Text(
+                    text = "Welcome to my App!",
+                    fontSize = 20.sp,
+                    color = Color.Yellow,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 80.dp)
+                        .background(Color.Black)
+                        .padding(20.dp)
+                )
+               Button(
+                   onClick = {
+
+                   },
+                   modifier = Modifier
+                       .padding(top=20.dp, start = 20.dp)
+                       .width(150.dp)
+                       .height(50.dp),
+                   shape = RoundedCornerShape(10.dp),
+                   colors = ButtonDefaults.buttonColors(
+                       containerColor = Color.Gray
+                   )
+
+
+               ) { Text(text = "GamePlay", fontSize = 18.sp) }
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AvacastTheme {
-        Greeting("Android")
-    }
-}
