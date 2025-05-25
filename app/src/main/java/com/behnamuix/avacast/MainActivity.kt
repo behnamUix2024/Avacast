a8e9a3086df2837d93a0501a274dda37ea208cbf
Behnam-6.403 <behnamuiux2023@gmail.com>
2025-05-25 12:56:07 +0330

    first commit

--- app/src/main/java/com/behnamuix/avacast/MainActivity.kt
+++ app/src/main/java/com/behnamuix/avacast/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,17 +21,26 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -35,42 +48,102 @@ import androidx.compose.ui.unit.sp
 import com.behnamuix.avacast.ui.theme.AvacastTheme
 
 class MainActivity : ComponentActivity() {
+    val list_color = listOf(Color.Green, Color.Blue, Color.Cyan)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column {
-                //oriantation:Vertical
+            Column(
+
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 100.dp),
+            ) {
                 Text(
-                    text = "Welcome to my App!",
-                    fontSize = 20.sp,
-                    color = Color.Yellow,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 80.dp)
-                        .background(Color.Black)
-                        .padding(20.dp)
+
+                    buildAnnotatedString {
+                        withStyle(
+                            SpanStyle(
+                                fontSize = 30.sp
+
+                            )
+                        ) {
+                            append("What is your")
+                        }
+                        withStyle(
+                            SpanStyle(
+                                brush = Brush.linearGradient(list_color),
+                                fontSize = 40.sp
+                            )
+                        ) {
+                            append(" name:")
+                        }
+                    }, modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
                 )
-               Button(
-                   onClick = {
+                OutlinedButton (
+                    onClick = {},
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .width(180.dp)
+                        .align(Alignment.CenterHorizontally)
 
-                   },
-                   modifier = Modifier
-                       .padding(top=20.dp, start = 20.dp)
-                       .width(150.dp)
-                       .height(50.dp),
-                   shape = RoundedCornerShape(10.dp),
-                   colors = ButtonDefaults.buttonColors(
-                       containerColor = Color.Gray
-                   )
+                ) {
+                    Text(
+                        buildAnnotatedString {
+                            withStyle(
+                                SpanStyle(
+                                    brush = Brush.radialGradient(list_color)
+                                )
+                            ){
+                                append("O")
+                            }
+                            withStyle( SpanStyle(
+                                brush = Brush.radialGradient(list_color)
+                            )){
+                                append("K")
+                            }
+                        }, modifier = Modifier
+                            .padding(10.dp)
 
 
-               ) { Text(text = "GamePlay", fontSize = 18.sp) }
-
-
+                    )
+                }
             }
+
         }
     }
+    @Preview(
+        showBackground = true,
+        name = "Test",
+        widthDp =300,
+        heightDp = 100
+    )
+    @Composable
+    fun Preview(){
+        Text(
+
+            buildAnnotatedString {
+                withStyle(
+                    SpanStyle(
+                        fontSize = 30.sp
+
+                    )
+                ) {
+                    append("What is your")
+                }
+                withStyle(
+                    SpanStyle(
+                        brush = Brush.linearGradient(list_color),
+                        fontSize = 40.sp
+                    )
+                ) {
+                    append(" name:")
+                }
+            }
+        )
+    }
 }
+
 
